57739089aaefd8733ed3481cbd28020613944835
刘兴雷 <whohasgone@gmail.com>
2017-12-21 00:25:25 +0800

    基本架构-未完成

--- Extrack_PD_RAW_From_Pure_Raw/main.cpp
+++ Extrack_PD_RAW_From_Pure_Raw/main.cpp
@@ -1,1 +1,46 @@
 #include <iostream>
+using namespace std;
+class PD
+{
+public:
+	PD();
+	~PD();
+
+private:
+
+};
+
+PD::PD()
+{
+}
+
+PD::~PD()
+{
+}
+void main()
+{
+	
+}
+
+int iniConfig()
+{
+	//初始化配置类
+
+}
+
+int getConfig()
+{
+	//获取配置文件并解析
+
+}
+
+int extractFromPDRAW()
+{
+	//提取buffer中的PD点
+
+}
+
+int writeIntoFile()
+{
+	//写入文件
+}
